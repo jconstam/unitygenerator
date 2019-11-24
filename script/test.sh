bf4d81f38d23210e24ebc342f997edc4383b060a
Jason Constam <callmebob@gmail.com>
2019-11-24 19:54:19 +0100

    Minor script changes.

--- script/test.sh
+++ script/test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-mkdir -p test/build
-mkdir -p test/unittest
+echo "====================="
+echo "CLEAN UP"
+echo "====================="
+rm -rfv test/build
+rm -rfv test/unittest
+echo "====================="
+echo "CREATING DIRECTORIES"
+echo "====================="
+mkdir -pv test/build
+mkdir -pv test/unittest
+echo "====================="
+echo "GENERATING PROJECT"
+echo "====================="
 python3 unityGenerator.py -s test/src -i test/include -t test/unittest
+echo "====================="
+echo "RUNNING CMAKE"
+echo "====================="
 cd test/build
 cmake ../unittest
+echo "====================="
+echo "BUILDING"
+echo "====================="
 make
