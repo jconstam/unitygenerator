f0ee2a14f8cb1994d6ce90717baec064ca8c9d60
Jason Constam <callmebob@gmail.com>
2019-12-29 15:30:59 -0700

    Added tests for file generation

--- tests/test_filemgr.py
+++ tests/test_filemgr.py
@@ -16,6 +16,39 @@ def setup_function( ):
 def teardown_function( ):
     common.cleanupRoot( )
 
+def setupTestFiles( ):
+    common.setupPath( sourcePath )
+    common.setupPath( os.path.join( sourcePath, 'folder1' ) )
+    common.setupPath( includePath )
+    common.setupPath( os.path.join( includePath, 'folder2' ) )
+
+    common.touchFile( os.path.join( sourcePath, 'fileA.c' ) )
+    common.touchFile( os.path.join( sourcePath, 'fileB.c' ) )
+    common.touchFile( os.path.join( sourcePath, 'fileC.c' ) )
+    common.touchFile( os.path.join( sourcePath, 'fileF.c' ) )
+    common.touchFile( os.path.join( sourcePath, 'folder1', 'fileE.c' ) )
+
+    common.touchFile( os.path.join( includePath, 'fileA.h' ) )
+    common.touchFile( os.path.join( includePath, 'fileB.h' ) )
+    common.touchFile( os.path.join( includePath, 'fileD.h' ) )
+    common.touchFile( os.path.join( includePath, 'fileE.h' ) )
+    common.touchFile( os.path.join( includePath, 'folder2', 'fileF.h' ) )
+
+    modules = [ ]
+    modules.append( createTestCModule( 'fileA.c', 'fileA', 'fileA.h' ) )
+    modules.append( createTestCModule( 'fileB.c', 'fileB', 'fileB.h' ) )
+    modules.append( createTestCModule( 'fileC.c', 'fileC', '' ) )
+    modules.append( createTestCModule( 'folder1/fileE.c', 'fileE', 'fileE.h' ) )
+    modules.append( createTestCModule( 'fileF.c', 'fileF', 'folder2/fileF.h' ) )
+    includeFiles = [ ]
+    includeFiles.append( 'fileA.h' )
+    includeFiles.append( 'fileB.h' )
+    includeFiles.append( 'fileD.h' )
+    includeFiles.append( 'fileE.h' )
+    includeFiles.append( 'folder2/fileF.h' )
+
+    return [ modules, includeFiles ]
+    
 def createTestCModule( sourceFile, moduleName, includeFile ):
     mod = filemgr.c_module( sourceFile, [ includeFile ] )
     mod.__sourceFile__ = sourceFile
@@ -44,42 +77,62 @@ def test_constructorNoFiles( ):
     assert not mgr.__modules__
     assert not mgr.__includeFiles__
 def test_constructor( ):
-    common.setupPath( sourcePath )
-    common.setupPath( os.path.join( sourcePath, 'folder1' ) )
-    common.setupPath( includePath )
-    common.setupPath( os.path.join( includePath, 'folder2' ) )
-
-    common.touchFile( os.path.join( sourcePath, 'fileA.c' ) )
-    common.touchFile( os.path.join( sourcePath, 'fileB.c' ) )
-    common.touchFile( os.path.join( sourcePath, 'fileC.c' ) )
-    common.touchFile( os.path.join( sourcePath, 'fileF.c' ) )
-    common.touchFile( os.path.join( sourcePath, 'folder1', 'fileE.c' ) )
-
-    common.touchFile( os.path.join( includePath, 'fileA.h' ) )
-    common.touchFile( os.path.join( includePath, 'fileB.h' ) )
-    common.touchFile( os.path.join( includePath, 'fileD.h' ) )
-    common.touchFile( os.path.join( includePath, 'fileE.h' ) )
-    common.touchFile( os.path.join( includePath, 'folder2', 'fileF.h' ) )
-
-    fileAModule = createTestCModule( 'fileA.c', 'fileA', 'fileA.h' )
-    fileBModule = createTestCModule( 'fileB.c', 'fileB', 'fileB.h' )
-    fileCModule = createTestCModule( 'fileC.c', 'fileC', '' )
-    fileDModule = createTestCModule( 'fileD.c', 'fileD', 'fileD.h' )
-    fileEModule = createTestCModule( 'folder1/fileE.c', 'fileE', 'fileE.h' )
-    fileFModule = createTestCModule( 'fileF.c', 'fileF', 'folder2/fileF.h' )
+    [ testModules, includeFiles ] = setupTestFiles( )
 
     mgr = filemgr.filemgr( sourcePath, includePath )
-    assert len( mgr.__modules__ ) == 5
-    assert fileAModule in mgr.__modules__
-    assert fileBModule in mgr.__modules__
-    assert fileCModule in mgr.__modules__
-    assert not fileDModule in mgr.__modules__
-    assert fileEModule in mgr.__modules__
-    assert fileFModule in mgr.__modules__
+    assert len( mgr.__modules__ ) == len( testModules )
+    for mod in testModules:
+        assert mod in mgr.__modules__
     
-    assert len( mgr.__includeFiles__ ) == 5
-    assert 'fileA.h' in mgr.__includeFiles__
-    assert 'fileB.h' in mgr.__includeFiles__
-    assert 'fileD.h' in mgr.__includeFiles__
-    assert 'fileE.h' in mgr.__includeFiles__
-    assert 'folder2/fileF.h' in mgr.__includeFiles__
+    assert len( mgr.__includeFiles__ ) == len( includeFiles )
+    for file in includeFiles:
+        assert file in mgr.__includeFiles__
+
+def test_createTestStubs( ):
+    testRootPath = common.createTestPath( 'testRoot' )
+    [ testModules, includeFiles ] = setupTestFiles( )
+
+    mgr = filemgr.filemgr( sourcePath, includePath )
+    mgr.createTestStubs( testRootPath )
+
+    for mod in testModules:
+        assert os.path.exists( mod.testStubPath( testRootPath ) )
+def test_createTestStubsPathExists( ):
+    testRootPath = common.createTestPath( 'testRoot' )
+    common.setupPath( testRootPath )
+    [ testModules, includeFiles ] = setupTestFiles( )
+    for mod in testModules:
+        os.makedirs( os.path.dirname( mod.testStubPath( testRootPath ) ) )
+
+    mgr = filemgr.filemgr( sourcePath, includePath )
+    mgr.createTestStubs( testRootPath )
+
+    for mod in testModules:
+        assert os.path.exists( mod.testStubPath( testRootPath ) )
+def test_createTestStubsFileExists( ):
+    testRootPath = common.createTestPath( 'testRoot' )
+    common.setupPath( testRootPath )
+    [ testModules, includeFiles ] = setupTestFiles( )
+    for mod in testModules:
+        os.makedirs( os.path.dirname( mod.testStubPath( testRootPath ) ) )
+        common.touchFile( mod.testStubPath( testRootPath ) )
+
+    mgr = filemgr.filemgr( sourcePath, includePath )
+    mgr.createTestStubs( testRootPath )
+
+    for mod in testModules:
+        assert os.path.exists( mod.testStubPath( testRootPath ) )
+
+def test_createTestCMakeList( ):
+    testRootPath = common.createTestPath( 'testRoot' )
+    common.setupPath( testRootPath )
+    [ testModules, includeFiles ] = setupTestFiles( )
+
+    mgr = filemgr.filemgr( sourcePath, includePath )
+    mgr.createTestCMakeList( testRootPath, sourcePath, includePath )
+
+    assert os.path.exists( os.path.join( testRootPath, 'CMakeLists.txt' ) )
+    
+    mgr.createTestCMakeList( testRootPath, sourcePath, includePath )
+
+    assert os.path.exists( os.path.join( testRootPath, 'CMakeLists.txt' ) )
